df7fc2ae3d7f83254ad9b52fc0b362cbd573011b
mstrong <mstrong1341@gmail.com>
2016-12-26 20:29:20 -0500

    not losing my streak because damn github doesnt show commits to forked repos

--- gokit_log_example/main.go
+++ gokit_log_example/main.go
@@ -11,4 +11,5 @@ func main() {
 	logger := kitlog.NewJSONLogger(os.Stdout)
 	stdlog.SetOutput(kitlog.NewStdlibAdapter(logger))
 	stdlog.Print("I sure like pie")
+	stdlog.Print("I wish commits to forked repos showed up in github profiles")
 }
