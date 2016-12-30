8ea4c0aca77e9e59a1a4c0de99d121966ac2f9a8
mstrong <mstrong1341@gmail.com>
2016-12-29 21:18:07 -0500

    Adding divide function

--- first_class_funcs/calculator_example/optimized_first_class_funcs/main.go
+++ first_class_funcs/calculator_example/optimized_first_class_funcs/main.go
@@ -30,19 +30,26 @@ func Mul(n float64) func(float64) float64 {
 	}
 }
 
-func main() {
-	var c Calculator
-	fmt.Println(c.Do(Add(5)))
-	fmt.Println(c.Do(Sub(3)))
-	fmt.Println(c.Do(Mul(8)))
-
-	var c2 Calculator
-	fmt.Println(c2.Do(Add(16)))
-	fmt.Println(c2.Do(Sqrt()))
+func Div(n float64) func(float64) float64 {
+	return func(acc float64) float64 {
+		return acc / n
+	}
 }
 
 func Sqrt() func(float64) float64 {
 	return func(n float64) float64 {
 		return math.Sqrt(n)
 	}
+}
+
+func main() {
+	var c Calculator
+	fmt.Println(c.Do(Add(5)))
+	fmt.Println(c.Do(Sub(3)))
+	fmt.Println(c.Do(Mul(8)))
+	fmt.Println(c.Do(Div(2)))
+
+	var c2 Calculator
+	fmt.Println(c2.Do(Add(16)))
+	fmt.Println(c2.Do(Sqrt()))
 }
