ba7133b0352f8404bbb6d1b0ef1026dbec31062f
mstrong <mstrong1341@gmail.com>
2017-01-01 22:53:46 -0500

    adding another func to goji generic API example

--- generic_api/main.go
+++ generic_api/main.go
@@ -8,10 +8,11 @@ import (
 	"goji.io/pat"
 	"golang.org/x/net/context"
 )
- 
+
 func main() {
 	mux := goji.NewMux()
 	mux.HandleFuncC(pat.Get("/hello/:name"), hello)
+	mux.HandleFuncC(pat.Get("/goodbye/:name"), goodbye)
 
 	http.ListenAndServe(":8000", mux)
 }
@@ -19,4 +20,9 @@ func main() {
 func hello(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	name := pat.Param(ctx, "name")
 	fmt.Printf("hello %s!", name)
+}
+
+func goodbye(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(ctx, "name")
+	fmt.Printf("Goodbye %s!", name)
 }
