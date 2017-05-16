80d04ee0153755e46c5ffe56a0cdb1a9857911ed
mstrong <mstrong1341@gmail.com>
2017-05-15 20:51:28 -0400

    clock1 example

---
+++ go_programming_language/chapter_8/clock1/main.go
@@ -0,0 +1,45 @@
+// Clock1 is a TCP severthat periodically writes the time
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// go build -o clock1
+// ./clock1 &
+// nc localhost 8000
+// 20:50:18
+// 20:50:19
+// 20:50:20
+// 20:50:21
+
+// note this does not stop the app on the port. Have to kill the PID
