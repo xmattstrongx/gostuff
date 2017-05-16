2703c93c3ad15a1c04aabc416afc2328725d6f74
mstrong <mstrong1341@gmail.com>
2017-05-15 21:16:10 -0400

    re-organizing. Adding reverb example

---
+++ go_programming_language/chapter_8/reverb_example/reverb1/main.go
@@ -0,0 +1,26 @@
+package reverb1
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Println(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Println(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Println(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
